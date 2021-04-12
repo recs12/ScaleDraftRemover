c43f0a5350aab24e27bc2c4a47fa708c21acf600
Slimane Rechdi - RECS <recs@premiertech.com>
2021-04-12 14:29:39 -0400

    add files

---
+++ README.md
@@ -0,0 +1,19 @@
+# ScaleDraftRemover
+recs 2021-04-12
+
+Use:
+=====
+ this macro delete automatically the scale indication in Solidedge Draft.
+
+
+This macro is written in IronPython, IronPython is an implementation of Python for the .NET.
+this is because the API solidedge only support .NET language, although this app could have
+been written in any language that support the .NET.
+
+Check the gifs in the folder ./docs to see how to use it.
+
+You can download IronPython here:
+https://ironpython.net/
+
+API solidedge:
+http://support.industrysoftware.automation.siemens.com/training/se/107/api/webframe.html
---
+++ __main__.py
@@ -0,0 +1,64 @@
+""" Replace background with the official of the company.
+"""
+
+import sys
+import clr
+import System
+from System import Console
+from System.IO.Path import Combine
+import System.Runtime.InteropServices as SRI
+import delete
+
+clr.AddReference("System")
+clr.AddReference("System.IO")
+clr.AddReference("Interop.SolidEdge")
+clr.AddReference("System.Runtime.InteropServices")
+
+__project__ = "ScaleDraftRemover"
+__author__ = "recs"
+__version__ = "0.0.1"
+__update__ = "2021-04-12"
+
+
+def prompt_exit():
+    raw_input("\nPress any key to exit...")
+    sys.exit()
+
+
+def raw_input(message):
+    Console.WriteLine(message)
+    return Console.ReadLine()
+
+
+def username():
+    return System.Environment.UserName
+
+
+def main():
+    """User can choose to update one draft or many in same time"""
+    try:
+        # Connection to the application, only one session should be opened here.
+        application = SRI.Marshal.GetActiveObject("SolidEdge.Application")
+        response = raw_input(
+            """Would you like to delete the scale indication, (Press y/[Y] to proceed.):\n"(Option: Press '*' for processing documents in batch)""")
+
+        if response.lower() in ["y", "yes"]:
+            doc = application.ActiveDocument
+            delete.background(doc)
+
+        elif response.lower() in ["*"]:
+            # loop through all the drafts
+            documents = application.Documents
+            for doc in documents:
+                delete.background(doc)
+        else:
+            pass
+
+    finally:
+        prompt_exit()
+
+
+if __name__ == "__main__":
+    print("%s\n--author: %s --version: %s --last-update : %s \n" %
+          (__project__, __author__, __version__, __update__))
+    main()
