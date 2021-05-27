60a69bdf926f44bfb644e892ba3e7c71f398b443
Slimane RECHDI <47180175+recs12@users.noreply.github.com>
2021-05-27 15:40:05 -0400

    modif compile

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 :: Set this file for compiling the executable.
 :: So it can be added to the user custom theme in solidedge.
 ipyc.exe /main:__main__.py ^
-delete.py ^
+scale.py ^
 Interop.SolidEdge.dll ^
 /embed ^
 /out:ScaleDraftRemover ^
